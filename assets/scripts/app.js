dd39f0c76e24cd2de98e4d22e64d8de7ec3970f8
xnIghtRain <32869515+xnIghtRain@users.noreply.github.com>
2020-07-18 17:25:53 +0200

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,45 +1,45 @@
-const Portfolio = function() {
+const Portfolio = (function () {
 	function makeWords() {
 		var words = [
 			{
-				text: "rapping",
-				weight: 12.3
-			}, {
+				text: "c#",
+				weight: 10.3,
+			},
+			{
 				text: "css3",
-				weight: 8
-			}, {
+				weight: 8,
+			},
+			{
 				text: "javascript",
-				weight: 14
-			}, {
-				text: "jquery",
-				weight: 3
-			}, {
+				weight: 7,
+			},
+			{
+				text: "c++",
+				weight: 9,
+			},
+			{
 				text: "programming",
-				weight: 7
-			}, {
+				weight: 7,
+			},
+			{
 				text: "python",
-				weight: 10
-			}, {
+				weight: 12,
+			},
+			{
 				text: "java",
-				weight: 9
-			}, {
-				text: "qazi",
-				weight: 15
-			}, {
-				text: "newText",
-				weight: 7
-			}
+				weight: 10.5,
+			},
 		];
 		return words;
 	}
 
 	function makeWordCloud(words) {
-		$('.teaching-domains').jQCloud(words, {delay: 120});
+		$(".teaching-domains").jQCloud(words, { delay: 120 });
 	}
 
 	function displayWordCloud() {
 		var count = 1;
-		$(window).on('scroll', function() {
+		$(window).on("scroll", function () {
 			var y_scroll_pos = window.pageYOffset;
 			var scroll_pos_test = 2700; // set to whatever you want it to be
 			var words = makeWords();
@@ -57,28 +57,32 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
+				"habe Spaß daran, neue Dinge zu lernen.",
+				"mag es, Sachen zu automatisieren",
+				"filler.",
+				"löse Probleme.",
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
 			// typing speed
 			typeSpeed: 1,
-			contentType: 'text',
-			callback: function() {
-				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			contentType: "text",
+			callback: function () {
+				$("#writing-text").css({
+					color: "#fff",
+					"background-color": "#C8412B",
+				});
 			},
-			preStringTyped: function() {},
-			onStringTyped: function() {}
+			preStringTyped: function () {},
+			onStringTyped: function () {},
 		});
 	}
 
 	return {
 		displayWordCloud: displayWordCloud,
-		typeAnimation: typeAnimation
-	}
-
-}();
-
+		typeAnimation: typeAnimation,
+	};
+})();
 
 Portfolio.displayWordCloud();
 Portfolio.typeAnimation();
